c9914d4db628895a629d73b4e179eaf950b26534
saheeb_tareque <saheebtareque97@gmail.com>
2026-03-12 14:52:28 +0600

    select statement added

---
+++ 1. SELECT_Statement.sql
@@ -0,0 +1,5 @@
+select * from parks_and_recreation.employee_demographics;
+
+select first_name,last_name,birth_date,age from parks_and_recreation.employee_demographics;
+
+select distinct gender,first_name from employee_demographics;
