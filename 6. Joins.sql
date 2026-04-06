ea3e83a3bb8f0e4b96dc0fbd17eb5a6bbef42e6b
saheeb_tareque <saheebtareque97@gmail.com>
2026-04-07 01:46:57 +0600

    updated with self join and also three table join

--- 6. Joins.sql
+++ 6. Joins.sql
@@ -12,3 +12,46 @@ on dem.employee_id = sal.employee_id;
 select *
 from employee_demographics dem right join employee_salary as sal
 on dem.employee_id = sal.employee_id;
+
+-- self join
+
+select * from employee_salary emp1
+join employee_salary emp2
+on emp1.employee_id +1 = emp2.employee_id;
+
+SELECT emp1.employee_id as emp_santa, emp1.first_name as santa_first_name, emp1.last_name as santa_last_name, emp2.employee_id, emp2.first_name, emp2.last_name
+FROM employee_salary emp1
+JOIN employee_salary emp2
+	ON emp1.employee_id + 1  = emp2.employee_id
+    ;
+
+-- So leslie is Ron's secret santa and so on -- Mark Brandanowitz didn't get a secret santa, but he doesn't deserve one because he broke Ann's heart so it's all good
+
+
+
+
+
+
+-- Joining multiple tables
+
+-- now we have on other table we can join - let's take a look at it
+SELECT * 
+FROM parks_and_recreation.parks_departments;
+
+
+SELECT *
+FROM employee_demographics dem
+INNER JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id
+JOIN parks_departments dept
+	ON dept.department_id = sal.dept_id;
+
+-- now notice when we did that, since it's an inner join it got rid of andy because he wasn't a part of any department
+
+-- if we do a left join we would still include him because we are taking everything from the left table which is the salary table in this instance
+SELECT *
+FROM employee_demographics dem
+INNER JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id
+LEFT JOIN parks_departments dept
+	ON dept.department_id = sal.dept_id;
